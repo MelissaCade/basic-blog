516d5b2cb620fa65d0cec787fc74b1a90429933f
BookWyrmMom <160056156+BookWyrmMom@users.noreply.github.com>
2024-03-07 10:02:42 -0700

    added css, changed mode toggle, added structure to blog html

---
+++ assets/js/form.js
@@ -0,0 +1,7 @@
+const postButton = document.querySelector("#post");
+
+//eventlistener to "hear" click on post button
+//store input on logic.js?
+//redirect to log page
+
+// window.location.href = "blog url"
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,15 +1,20 @@
-const themeSwitcher = document.querySelector('#theme-switcher');
-const container = document.querySelector('.container');
+const themeSwitcher = document.querySelector("#theme-switcher");
+const container = document.querySelector(".container");
 
-let mode = 'dark';
+let mode = "dark";
 
-themeSwitcher.addEventListener('click', function () {
-  if (mode === 'dark') {
-    mode = 'light';
-    container.setAttribute('class', 'light');
-  }
-  else {
-    mode = 'dark';
-    container.setAttribute('class', 'dark');
+themeSwitcher.addEventListener("click", function () {
+  if (mode === "dark") {
+    mode = "light";
+    container.setAttribute("class", "light");
+    themeSwitcher.setAttribute("src", "./assets/images/moon.png");
+  } else {
+    mode = "dark";
+    container.setAttribute("class", "dark");
+    themeSwitcher.setAttribute("src", "./assets/images/sun.png");
   }
 });
+
+//event listener for click on post button
+//save input to local storage?
+//when redirected, put stuff on new page?
